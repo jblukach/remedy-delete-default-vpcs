0c150b3f64a81579254ece540a6c66c29d9e5ed2
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-01-29 15:18:28 +0000

    aws-cdk-lib==2.10.0

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = cdk.App()
 
 RemedyDeleteDefaultVpcsStack(
     app, 'RemedyDeleteDefaultVpcsStack',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = os.getenv('CDK_DEFAULT_REGION')
+    ),
     synthesizer = cdk.DefaultStackSynthesizer(
         qualifier = '4n6ir'
     )
